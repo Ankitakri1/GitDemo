c0b5003c423e1773e7a6d33034ae374287a9d465
ankita <nktkmr60@gmail.com>
2021-02-02 16:00:53 +0530

    actual changes cleaned

--- src/test/java/DemoBank/Login.java
+++ src/test/java/DemoBank/Login.java
@@ -10,14 +10,6 @@ public class Login {
 	
 
 	public static void main(String[] args) {
-
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\nktkm\\Downloads\\chromedriver_win32" + 
-				 "\\chromedriver.exe");
-
-		WebDriver driver = new ChromeDriver();
-
-		driver.get("http://www.demo.guru99.com/V4/");
-	public static void main(String[] args) throws InterruptedException {
 		
 		launchBrowser();
 		//enter username

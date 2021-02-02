44d249dd4b80939da67cad62f45e9970460677d9
ankita <nktkmr60@gmail.com>
2021-02-02 14:39:59 +0530

    reload

--- src/test/java/DemoBank/Login.java
+++ src/test/java/DemoBank/Login.java
@@ -7,32 +7,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Login {
 
 	public static void main(String[] args) {
-		
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\nktkm\\Downloads\\chromedriver_win32" + 
+				 "\\chromedriver.exe");
+
 		WebDriver driver = new ChromeDriver();
-		
-		//enter username
+
+		driver.get("http://www.demo.guru99.com/V4/");
 		driver.findElement(By.name("uid")).sendKeys("mngr306727");
-		
-		//enter password
 		driver.findElement(By.name("password")).sendKeys("emAruhE");
-		
-		//click login
 		driver.findElement(By.name("btnLogin")).click();
-		
-		//verify login successful	
 	}
-	
-	public WebDriver launchBrowser(WebDriver driver) {
-		//setup Chrome driver
-				System.setProperty("webdriver.chrome.driver", "C:\\Users\\nktkm\\Downloads\\chromedriver_win32" + 
-						 "\\chromedriver.exe");
-				driver = new ChromeDriver();
-				String baseUrl = "http://www.demo.guru99.com/V4/";
-				
-				//launch the browser
-				driver.get(baseUrl);
-				return driver;
-	}
-	
-	
 }

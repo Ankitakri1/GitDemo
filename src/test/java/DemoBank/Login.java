3bc481887925ac3c5e6a210b0d914543bef53ef7
ankita <nktkmr60@gmail.com>
2021-02-02 15:59:13 +0530

    commiting

--- src/test/java/DemoBank/Login.java
+++ src/test/java/DemoBank/Login.java
@@ -9,7 +9,6 @@ public class Login {
 	static WebDriver driver;
 	
 
-<<<<<<< HEAD
 	public static void main(String[] args) {
 
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\nktkm\\Downloads\\chromedriver_win32" + 
@@ -18,17 +17,14 @@ public class Login {
 		WebDriver driver = new ChromeDriver();
 
 		driver.get("http://www.demo.guru99.com/V4/");
-=======
 	public static void main(String[] args) throws InterruptedException {
 		
 		launchBrowser();
 		//enter username
->>>>>>> 31d22bb84adbae9ad085338ad791ea5916ebfd32
 		driver.findElement(By.name("uid")).sendKeys("mngr306727");
 		driver.findElement(By.name("password")).sendKeys("emAruhE");
 		driver.findElement(By.name("btnLogin")).click();
-<<<<<<< HEAD
-=======
+
 		
 		//verify login successful	
 	}
@@ -43,6 +39,5 @@ public class Login {
 				
 				//launch the browser
 				driver.get(baseUrl);
->>>>>>> 31d22bb84adbae9ad085338ad791ea5916ebfd32
 	}
 }
